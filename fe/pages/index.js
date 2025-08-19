a826c69597351a9496f778b2d8a62cf6bd9f5026
selmyaf <155378631+selmyaf@users.noreply.github.com>
2025-08-19 09:21:38 +0700

    Update navbar logic & login redirect

--- fe/pages/index.js
+++ fe/pages/index.js
@@ -1,16 +1,13 @@
 "use client";
-import { useState } from "react";
+import { useEffect } from "react";  // <--- impor useEffect
 import { useRouter } from "next/router";
-import axios from "axios";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace("/login"); 
+    router.replace("/login"); // redirect ke login
   }, [router]);
 
-  return null;
+  return null; // bisa kasih loading spinner kalau mau
 }
