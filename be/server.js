e5f38cda49ea753c81061dbea2a1d1bb37d2e934
selmyaf <155378631+selmyaf@users.noreply.github.com>
2025-08-19 16:44:06 +0700

    Update navbar logic & login redirect

--- be/server.js
+++ be/server.js
@@ -13,6 +13,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors({
+  origin: "https://inventory-project-rev-git-main-sells-projects.vercel.app", // domain vercel kamu
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+app.options("*", (req, res) => {
+  res.sendStatus(200);
+});
+
 // --- Upload Multer ---
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "uploads/"),
